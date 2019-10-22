b203785ad018d237069f71245ed65a623dbc3364
Bahir Ali Mahamadou Aminou <mahamadou208@gmail.com>
2019-10-22 01:19:07 +0100

    jointure table categorie avec magasin pour pouvoir afficher la categorie de chaque magasins

--- app/Http/Controllers/MagasinController.php
+++ app/Http/Controllers/MagasinController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Magasin;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MagasinController extends Controller
 {
     public function index(){
-        $magsin = Magasin::all();
+        // $magsin = Magasin::all();
+
+        $magsin = DB::table('categories')
+                    ->join('magasins','magasins.idCateg','=','categories.idCateg')
+                    ->select('magasins.nom','magasins.image','categories.nomCateg')
+                    ->get();
         return response()->json(
             [
                'magasin' => $magsin 
